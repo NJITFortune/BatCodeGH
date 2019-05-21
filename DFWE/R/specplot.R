76317cd192679c9ca8a467d9d2e8d99e0a0f7a93
Eric Fortune <eric.fortune@njit.edu>
2019-05-21 17:12:42 -0400

    / ‘DFWE/R/specplot.R’

--- DFWE/R/specplot.R
+++ DFWE/R/specplot.R
@@ -1,3 +1,5 @@
+#specplot
+
 #This function will plot spectrograms. It works with either a wav file read by tuneR or frequency data.
 #When using frequency data alone, you need to specify a sample frequency. This function relies on the
 #imagep() function from oce. specplot() uses custom color palletes with 30 colors. Most parameters have defaults
